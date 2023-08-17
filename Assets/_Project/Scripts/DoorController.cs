ee07066e5ee00b6c2e6767ad25663eea6ab600b4
Eong-Koungmeng <koung.meng12@gmail.com>
2023-08-17 09:42:01 +0700

    added door animation and script to open and close door when player is near(trigger box collider)

--- Assets/_Project/Scripts/DoorController.cs
+++ Assets/_Project/Scripts/DoorController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+    Animator animator;
+    bool isOpen = false;
+    bool inContact = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(inContact) 
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                if(isOpen)
+                {
+                    animator.Play("DoorClose");
+                    isOpen = false;
+                }
+                else
+                {
+                    animator.Play("DoorOpen");
+                    isOpen = true;
+                }
+            }
         
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inContact = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inContact = false;
     }
 }
